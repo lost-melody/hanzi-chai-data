8435923978eb80869ef4009dc928d85870ef4688
Songchen Tan <i@tansongchen.com>
2023-10-25 12:37:36 -0400

    Fix api name

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'itty-router';
 import { routerUsers } from './users';
 import { routerRepertoire } from './repertoire';
-import { routerForms } from './forms';
+import { routerForm } from './form';
 import { Login } from '../controller/users';
 
 /** 主路由, 以 `/api` 为前缀 */
-export const routerApi = Router({ base: "/api" })
+export const routerApi = Router()
 	// 登录接口
 	.post('/login', Login)
 	// 用户子路由
@@ -13,4 +13,4 @@ export const routerApi = Router({ base: "/api" })
 	// 待编码汉字子路由
 	.all('/repertoire/*', routerRepertoire.handle)
 	// 字形信息子路由
-	.all('/forms/*', routerForms.handle);
+	.all('/form/*', routerForm.handle);
